85fdab647f8621cd7b0b5cf561f190051c1b216c
GLisowski <glisowski@fractalsoft.org>
2016-02-26 17:07:09 +0100

    initial version, blue background

---
+++ MonsterTextured/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+
+namespace MonsterTextured
+{
+    public partial class Form1 : Form
+    {
+        Device device = null;
+
+        public Form1()
+        {
+            this.ClientSize = new System.Drawing.Size(400, 300);
+            this.Text = "D3D Tutorial 01: CreateDevice";
+        }
+
+        public bool InitializeDirect3D()
+        {
+            PresentParameters pp = new PresentParameters();
+
+            pp.Windowed = true;
+            pp.SwapEffect = SwapEffect.Discard;
+
+            try
+            {
+                device = new Device(0,
+                        DeviceType.Hardware,
+                        this,
+                        CreateFlags.SoftwareVertexProcessing,
+                        pp);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void Run()
+        {
+            while (this.Created)
+            {
+                GameLogic();
+                Render();
+                Application.DoEvents();
+            }
+        }
+
+        private void GameLogic()
+        {
+            // TODO: add your game logic here
+        }
+
+        private void Render()
+        {
+            device.Clear(ClearFlags.Target,
+                        Color.Blue, 1.0f, 0);
+            device.Present();
+        }
+
+    }
+}
---
+++ MonsterTextured/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace MonsterTextured
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            using (Form1 frm = new Form1())
+            {
+                if (!frm.InitializeDirect3D())
+                {
+                    MessageBox.Show("Error initizlizing Direct3D");
+                    return;
+                }
+                frm.Show();
+                frm.Run();
+            }
+        }
+    }
+}
